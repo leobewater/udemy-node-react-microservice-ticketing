a15ddd3125950220b0ff89a47aba58aece0d8a60
Leo <lcheung@swellbottle.com>
2022-01-26 07:32:17 -0500

    use express validator to validate sign up email and password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,15 +1,27 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
 
-  if( !email || typeof email !== 'string') {
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be bewtween 4 and 20 characters'),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    if (!email || typeof email !== 'string') {
       res.status(400).send('Provide a valid email');
-  }
+    }
 
-  res.send('Signup!');
-});
+    res.send('Signup!');
+  }
+);
 
 export { router as signupRouter };
