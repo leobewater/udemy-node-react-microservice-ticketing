3d70df49b19700807be4f772d63a01bc4beef62e
Leo <lcheung@swellbottle.com>
2022-01-26 15:08:12 -0500

    add basic sign in validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,37 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must provide a password'),
+  ],
+  (req: Request, res: Response) => {
+    // validate request body
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    // const { email, password } = req.body;
+
+    // // check for user existence
+    // const existingUser = await User.findOne({ email });
+    // if (existingUser) {
+    //   throw new BadRequestError('Email in use');
+    // }
+
     res.send('sign in');
-});
+  }
+);
 
 export { router as signinRouter };
