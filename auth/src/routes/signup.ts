359bc09bae057f6ff6fa17f40615bda47ae62271
Leo <lcheung@swellbottle.com>
2022-01-26 07:26:51 -0500

    add dummy auth routes

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/api/users/signup', (req, res) => {
+  const { email, password } = req.body;
+
+  if( !email || typeof email !== 'string') {
+      res.status(400).send('Provide a valid email');
+  }
+
+  res.send('Signup!');
+});
+
+export { router as signupRouter };
