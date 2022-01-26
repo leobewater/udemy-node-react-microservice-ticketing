ffca429eaed4bacccfe74b1d21a06f9bff1ead72
Leo <lcheung@swellbottle.com>
2022-01-26 14:09:46 -0500

    set up cookieSession in auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,15 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    // disable encryption on cookie
+    signed: false,
+    secure: true,
+  })
+);
 
 // routes
 app.use(currentUserRouter);
