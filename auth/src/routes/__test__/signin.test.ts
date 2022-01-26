70cdd03dc581fefa4e9fef2ffcb5b21b8eff246a
Leo <lcheung@swellbottle.com>
2022-01-26 17:57:08 -0500

    add sign in tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,52 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('fails when a email that does not exist is given', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('fails when an incorrect password is given', async () => {
+  // register user
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'wrong',
+    })
+    .expect(400);
+});
+
+it('responds with a cookie when given valid credentials given', async () => {
+  // register user
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
