a65bf40933883f3f7a4f742e2b04b1ad3522044a
Leo <lcheung@swellbottle.com>
2022-01-26 09:35:26 -0500

    add 404 route error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -15,10 +16,14 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+// 404 error
+app.get('*', () => {
+  throw new NotFoundError();
+});
 
 // middlewares
 app.use(errorHandler);
 
-app.listen(3000, ()=> {
-    console.log("Listening on port 3000!");
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
 });
