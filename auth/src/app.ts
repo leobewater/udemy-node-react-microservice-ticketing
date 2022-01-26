65d6f4a02954c645f0bb760a281f29040f8d4767
Leo <lcheung@swellbottle.com>
2022-01-26 17:06:55 -0500

    refactor and separate app to app.ts preparing for testing

---
+++ auth/src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    // disable encryption on cookie
+    signed: false,
+    secure: true,
+  })
+);
+
+// routes
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+// handle async error using next() without the express-async-errors plugin
+// app.all('*', async (req, res, next) => {
+//     next(new NotFoundError());
+// });
+
+// 404 error for all actions and async with plugin express-async-errors
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+// middlewares
+app.use(errorHandler);
+
+export { app };
