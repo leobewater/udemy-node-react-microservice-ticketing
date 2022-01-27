3aaa6b3cfc895c339b26dc62283605d9472b90fb
Leo <lcheung@swellbottle.com>
2022-01-27 13:37:23 -0500

    add tests for posting ticket title and price

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -14,15 +14,62 @@ it('can only be accessed if the user is signed in', async () => {
 it('returns a status other than 401 if the user is signed in', async () => {
   const response = await request(app)
     .post('/api/tickets')
-    .set('Cookie',  global.signin())
+    .set('Cookie', global.signin())
     .send({});
   // console.log(response.status);
   // console.log('response.body.currentUser', response.body.currentUser);
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
 
-it('returns an error if an invalid price is provided', async () => {});
+  // send without title
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  // price with neg value
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'title',
+      price: -10,
+    })
+    .expect(400);
+
+  // price at $0
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'title',
+      price: 0,
+    })
+    .expect(400);
+
+  // send without price
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'title',
+    })
+    .expect(400);
+});
 
 it('creates a ticket with valid inputs', async () => {});
