184b3f6221dcc0e68e24c3ec6aeb0e57bb192ce7
Leo <lcheung@swellbottle.com>
2022-01-27 13:37:07 -0500

    add ticket post request validation

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,22 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@mmb8npm/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@mmb8npm/common';
 
 const router = express.Router();
 
 // add requireAuth middleware
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').trim().not().isEmpty().withMessage('Title is required'),
+
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be great than 0'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
