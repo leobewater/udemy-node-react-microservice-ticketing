9608612477f8b9f8cdcb7e2789bba3d2a332886f
Leo <lcheung@swellbottle.com>
2022-01-27 05:39:30 -0500

    add getInitialProps to landing page which fetch data once on the server side and pass to the page components

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
-const Landing = () => {
-  return <h1>Landing page</h1>;
+const LandingPage = ({ color }) => {
+  console.log("im in the component", color);
+  return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+// fetch data once on the server side and pass them to the page components
+LandingPage.getInitialProps = () => {
+  console.log('Im on the server!');
+
+  return { color: 'red' };
+};
+
+export default LandingPage;
