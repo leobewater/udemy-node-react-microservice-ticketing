b48eafc0382b3fc6fd7d59440eeee79f3b5fecde
Leo <lcheung@swellbottle.com>
2022-01-27 17:23:58 -0500

    set subscription option to manually acknowledge the message was received

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -13,17 +13,27 @@ const stan = nats.connect('ticketing', clientId, {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  // .setManualAckMode(true) - NATs no longer auto ackownledge the message was received. 
+  // if no acknowledge was recieved, after 30s it will publish again.
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
   // listen to ticket:created topic/event with a Queue Group to avoid receiving duplicated message
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group'
+    'orders-service-queue-group',
+    options
   );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
+
     // check received data type
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data:${data}`);
     }
+
+    // manually acknowledge the message was received
+    msg.ack();
+
   });
 });
