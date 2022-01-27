d3c7c0760e23138d8c159fdd09c5e865a51c9eac
Leo <lcheung@swellbottle.com>
2022-01-27 11:18:54 -0500

    add requireAuth to tickets routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import { json } from 'body-parser';
 import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@mmb8npm/common';
+import { errorHandler, NotFoundError, currentUser } from '@mmb8npm/common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,7 +18,8 @@ app.use(
 );
 
 // routes
-// 404 error for all actions and async with plugin express-async-errors
+app.use(currentUser);
+app.use(createTicketRouter);
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,9 +1,15 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('has a route handler listening to /api/tickets for post request', async () => {});
+it('has a route handler listening to /api/tickets for post request', async () => {
+  const response = await request(app).post('/api/tickets').send({});
 
-it('can only be accessed if the user is signed in', async () => {});
+  expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).post('/api/tickets').send({}).expect(401);
+});
 
 it('returns an error if an invalid title is provided', async () => {});
 
