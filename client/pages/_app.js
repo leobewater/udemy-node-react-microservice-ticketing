d0bb8773c4794b52a9be00bfe73d76af1d5784e6
Leo <lcheung@swellbottle.com>
2022-01-27 07:34:34 -0500

    moving get current user axios from index.js to _app.js and also run any page component getInitialProps()

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,36 @@
-// load css for all pages
+// This is default page layout
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-// wrapper for component
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+// const LandingPage = ({ currentUser }) => {
+//   return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
+// };
+
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default App;
+// getInitialProps() executes on the server side on the first load which fetches data once on the server side and pass them to the page components.
+AppComponent.getInitialProps = async (appContext) => {
+  // console.log(appContext);
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  // also run any page component.getInitialProps()
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  // console.log(pageProps);
+  // console.log(data);
+
+  return data;
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -4,8 +4,9 @@ const LandingPage = ({ currentUser }) => {
   return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
 };
 
-// getInitialProps() executes on the server side on the first load which fetches data once on the server side and pass them to the page components.
+// if _app.js has .getInitialProps(), this.getInitialProps won't be running
 LandingPage.getInitialProps = async (context) => {
+  console.log('landing page!');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
   return data;
