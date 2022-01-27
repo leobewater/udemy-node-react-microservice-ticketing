fcc91bd5740183e30b42a4c38220f4443e94d851
Leo <lcheung@swellbottle.com>
2022-01-27 13:28:13 -0500

    create fake cookie for sign in test on ticket service

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 
-it('has a route handler listening to /api/tickets for post request', async () => {
+it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
 
   expect(response.status).not.toEqual(404);
@@ -9,6 +9,16 @@ it('has a route handler listening to /api/tickets for post request', async () =>
 
 it('can only be accessed if the user is signed in', async () => {
   await request(app).post('/api/tickets').send({}).expect(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie',  global.signin())
+    .send({});
+  // console.log(response.status);
+  // console.log('response.body.currentUser', response.body.currentUser);
+  expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {});
