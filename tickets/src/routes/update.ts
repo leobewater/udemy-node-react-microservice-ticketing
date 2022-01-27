e1e19c15ed6e7699d08a84fd08d6acbcd3dcf7a0
Leo <lcheung@swellbottle.com>
2022-01-27 15:40:03 -0500

    add update ticket route and logic

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -10,6 +10,36 @@ import { Ticket } from '../models/ticket';
 
 const route = express.Router();
 
-route.put('/api/tickets/:id', async (req: Request, res: Response) => {});
+route.put(
+  '/api/tickets/:id',
+  requireAuth,
+  [
+    body('title').trim().not().isEmpty().withMessage('Title is required'),
+
+    body('price').isFloat({ gt: 0 }).withMessage('Price must be great than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // check ticket userId with the logged current user id
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    // update the ticket
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
+
+    res.send(ticket);
+  }
+);
 
 export { route as updateTicketRouter };
