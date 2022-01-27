86d3e665f4f5163c4087d369b5267e3c16038d67
Leo <lcheung@swellbottle.com>
2022-01-26 22:19:24 -0500

    add condition only to show errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -41,14 +41,16 @@ export default () => {
           className="form-control"
         />
       </div>
-      <div className="alert alert-danger">
-        <h4>Ooops....</h4>
-        <ul className="my-0">
-          {errors.map((err) => (
-            <li key={err.message}>{err.message}</li>
-          ))}
-        </ul>
-      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops....</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <button className="btn btn-primary">Sign Up</button>
     </form>
