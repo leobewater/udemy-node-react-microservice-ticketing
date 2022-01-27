7c258749eb08bc67ff716bb92305eb5d1f20a7bf
Leo <lcheung@swellbottle.com>
2022-01-27 07:10:30 -0500

    add condition to show whether user is signed in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  return <h1>Landing Page {currentUser.email}</h1>;
+  return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
 };
 
 // getInitialProps() executes on the server side on the first load which fetches data once on the server side and pass them to the page components.
