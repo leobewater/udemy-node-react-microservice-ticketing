f76e295bb686cad1523651c996681ff241b9abac
Leo <lcheung@swellbottle.com>
2022-01-28 05:30:15 -0500

    passing TicketCreatedEvent interface to TicketCreatedListener to guide data value format

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,15 +1,19 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
   onMessage(data: any, msg: Message): void {
     console.log('Event data!', data);
 
-    // business logic, if fails, just let it times out 
+    // business logic, if fails, just let it times out
     // so it will queue back to the NATS streaming server and resend again
+    console.log(data.name);
+    console.log(data.cost);
 
     // if success, acknowledge message received back to NATS since the manual subscription option was enabled
     msg.ack();
