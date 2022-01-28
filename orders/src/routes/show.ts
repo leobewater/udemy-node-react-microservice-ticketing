9966d7699f9ea98a1d6cd462a91db7df8af52da2
Leo <lcheung@swellbottle.com>
2022-01-28 15:17:38 -0500

    add order show route logic

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,45 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@mmb8npm/common';
+import { body } from 'express-validator';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  '/api/orders/:orderId',
+  [
+    body('orderId')
+      .notEmpty()
+      // check reference orderId structure/format
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('OrderId must be provided'),
+  ],
+  requireAuth,
+  async (req: Request, res: Response) => {
+    /*
+    // you can find order with id and userid but you won't be able to throw 401 error
+    const order = await Order.findOne({
+      _id: req.params.orderId,
+      userId: req.currentUser!.id,
+    }).populate('ticket')
+    */
+
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId != req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
