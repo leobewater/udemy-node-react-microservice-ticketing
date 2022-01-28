dc2544462ec625024d9aa69b54549d9547511986
Leo <lcheung@swellbottle.com>
2022-01-28 14:14:36 -0500

    add isReserved method to ticket model

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose';
+import { Order, OrderStatus } from '../models/order';
 
 // duplicated model from Ticket service.
-// DON'T put this in the Common library since it's slight different.
+// DON'T put this in the Common library since it's slight different than Tickets Service -> Ticket Model.
 interface TicketAttrs {
   title: string;
   price: number;
 }
 
+// add isReserved method to TicketDoc
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -40,6 +43,22 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
+};
+
+ticketSchema.methods.isReserved = async function () {
+  // this === the ticket document that we just called 'isReserved' on
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder;
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
