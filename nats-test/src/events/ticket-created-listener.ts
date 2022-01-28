68d972cdc9e3bcc67ddfa858680968656c9d7e7e
Leo <lcheung@swellbottle.com>
2022-01-28 05:36:05 -0500

    use TS event interface to set an onmessage data type annotations

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,18 +4,20 @@ import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
     console.log('Event data!', data);
 
-    // business logic, if fails, just let it times out
-    // so it will queue back to the NATS streaming server and resend again
-    console.log(data.name);
-    console.log(data.cost);
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     // if success, acknowledge message received back to NATS since the manual subscription option was enabled
     msg.ack();
+
+    // if business logic fails, just let it times out
+    // so it will queue back to the NATS streaming server and resend again
   }
 }
