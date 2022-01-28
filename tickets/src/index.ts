ecb1f1cfdcb7b0bbbc7165fd543e7a5684205531
Leo <lcheung@swellbottle.com>
2022-01-28 08:19:36 -0500

    initialize nats client in app.js

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+import { randomBytes } from 'crypto';
 
 // connect mongo and start server
 // latest node must have an async function to start, await can't be at the top level
@@ -13,6 +15,14 @@ const start = async () => {
   }
 
   try {
+    // "ticketing" is from the -cid config in nats-depl.yaml
+    await natsWrapper.connect(
+      'ticketing',
+      randomBytes(4).toString('hex'),
+      'http://nats-srv:4222'
+    );
+    console.log('Connected to NATS Streaming');
+
     // using deployment host name and connect to auth db
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
