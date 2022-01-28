aa325e1589db9dd8f9de443c0847b9319b634dbd
Leo <lcheung@swellbottle.com>
2022-01-28 05:24:44 -0500

    add interface Event and make it a generic type for abstract class Listener

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,10 +1,17 @@
 // TS abstract class
 import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
 
-export abstract class Listener {
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+// make it a generic class
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000; // 5s
 
