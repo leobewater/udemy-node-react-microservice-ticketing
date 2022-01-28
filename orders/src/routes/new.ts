df6819562e2434877247e8bf1bc742de49f10890
Leo <lcheung@swellbottle.com>
2022-01-28 14:20:41 -0500

    finish up new order logic

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -12,6 +12,9 @@ import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
+// ticket expires in 15 mins or move it to env var, different time for other user roles
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
 router.post(
   '/api/orders',
   requireAuth,
@@ -41,12 +44,23 @@ router.post(
     }
 
     // Calculate an expiration date (default +15 mins) for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // TODO - use Database Transactions
 
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
 
     // Dispatch an event saying that an order was created
 
-    res.send({});
+    res.status(201).send(order);
   }
 );
 
