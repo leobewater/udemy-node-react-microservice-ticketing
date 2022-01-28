a98d22a63b7a2a892f23a8066a4dfe880a52bfb8
Leo <lcheung@swellbottle.com>
2022-01-28 08:25:58 -0500

    add a TS getter to return the client

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,21 +1,31 @@
 import nats, { Stan } from 'node-nats-streaming';
 
+// create a wrapper to share a singleton client via the app
 class NatsWrapper {
   // "?" = this property will be initialized later
   private _client?: Stan;
+
+  // use Typescript getter
+  get client() {
+    if (!this._client) {
+      throw new Error('Cannot access NATS client before connecting');
+    }
+
+    return this._client;
+  }
 
   connect(clusterId: string, clientId: string, url: string) {
     this._client = nats.connect(clusterId, clientId, { url });
 
     // return a Promise
     return new Promise<void>((resolve, reject) => {
-      this._client!.on('connect', () => {
+      this.client.on('connect', () => {
         console.log('Connected to NATS');
         resolve();
       });
 
       // fail to connect, return reject
-      this._client!.on('error', (err) => {
+      this.client.on('error', (err) => {
         reject(err);
       });
     });
