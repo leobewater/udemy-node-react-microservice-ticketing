70ef6e671b957eab5180950482627405ffed96c4
Leo <lcheung@swellbottle.com>
2022-01-28 09:50:31 -0500

    add jest.fn to the mocked function so we can add assertion

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,8 +1,13 @@
 // mock the natsWrapper for Jest tests without connecting to the actual NATS server
 export const natsWrapper = {
   client: {
-    publish: (subject: string, data: string, callback: () => void) => {
-      callback();
-    },
+    // use jest.fn().mockImplementation() so we can write assertion in our tests
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
   },
 };
