0230fc48838b9e249d5691999afc3bf1d73e3462
Leo <lcheung@swellbottle.com>
2022-01-29 14:51:51 -0500

    add job to expiration queue after received order:created event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,17 +1,18 @@
-import {
-  Listener,
-  OrderCreatedEvent,
-  OrderStatus,
-  Subjects,
-} from '@mmb8npm/common';
+import { Listener, OrderCreatedEvent, Subjects } from '@mmb8npm/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject: Subjects.OrderCreated = Subjects.OrderCreated;
   readonly queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    // { id: string; version: number; status: OrderStatus; userId: string; expiresAt: string; ticket: { id: string; price: number; }; }
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // when received the order:created event, then queue a job to the Redis server for expiration
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
   }
 }
