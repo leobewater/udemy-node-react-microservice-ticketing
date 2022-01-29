3637ba1c911975d39ec5114c43d9da32f00bac73
Leo <lcheung@swellbottle.com>
2022-01-29 18:55:47 -0500

    initialize payments service with Tickets setup files

---
+++ payments/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@mmb8npm/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    // disable encryption on cookie
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+// routes
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+// middlewares
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,71 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+// tell jest to use the mock file in the __mocks__ folder
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+
+/*
+ * Test hooks
+ */
+// create in-memory mongodb server before all tests
+beforeAll(async () => {
+  // hardcode env vars for testing
+  process.env.JWT_KEY = 'asdfasdf';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+// delete all database collections and jest mocks before running each test
+beforeEach(async () => {
+  jest.clearAllMocks();
+  
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// stop mongodb after running all tests
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+/*
+ * Global Test Functions
+ */
+declare global {
+  var signin: () => string[];
+}
+
+global.signin = () => {
+  // Create a fake cookie, for reference, you can signup and find the currentUser cookie response
+
+  // Build a JWT payload and randomly generate the id. { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
