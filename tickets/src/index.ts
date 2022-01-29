a0736c3641382e4766d79764cbcf488be74c7322
Leo <lcheung@swellbottle.com>
2022-01-29 13:36:08 -0500

    init order created/cancelled listeners in Tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 // connect mongo and start server
 // latest node must have an async function to start, await can't be at the top level
@@ -36,6 +38,10 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    // init listeners
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     // using deployment host name and connect to auth db
     await mongoose.connect(process.env.MONGO_URI);
