05883756931abdcd7e7b30cd5f9125a0464360b2
Leo <lcheung@swellbottle.com>
2022-01-29 18:45:44 -0500

    clean up

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -24,7 +24,7 @@ const start = async () => {
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
-  
+
   try {
     await natsWrapper.connect(
       process.env.NATS_CLUSTER_ID,
