63736065e341c79b3c3652686a7a26bdd7618870
Leo <lcheung@swellbottle.com>
2022-01-29 10:22:55 -0500

    add ticket updated listener tests

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { TicketUpdatedEvent } from '@mmb8npm/common';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  // create a fake ticket data event
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: 'new concert',
+    price: 999,
+    userId: 'fakeuser',
+  };
+
+  // create a fake ticket message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, ticket };
+};
+
+it('finds, updates, and saves a ticket', async () => {
+  const { listener, data, msg, ticket } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // fetch ticket from the collection
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  // write assertions to make sure a ticket was updated!
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the mssage', async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack() function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
