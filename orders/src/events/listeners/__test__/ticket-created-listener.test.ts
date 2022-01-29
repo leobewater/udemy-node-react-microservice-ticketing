b755726d37ca1f4050e5beba5ee334c6dc883faf
Leo <lcheung@swellbottle.com>
2022-01-29 09:23:21 -0500

    add ticket created listener test boilerplate

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,16 @@
+
+const setup = async() => {
+  // create an instance of the listener
+  // create a fake ticket data event
+  // create a fake ticket message object
+};
+
+it('creates and saves a ticket', async () => {
+  // call the onMessage function with the data object + message object
+  // write assertsion to make sure a ticket was created!
+});
+
+it('acknowledges the message', async () => {
+  // call the onMessage function with the data object + message object
+  // write assertsion to make sure ack() function is called
+});
