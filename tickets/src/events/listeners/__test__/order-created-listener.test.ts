c2d552d5116fbb8e7aadbb7b17949ed3be3c919d
Leo <lcheung@swellbottle.com>
2022-01-29 11:37:03 -0500

    add order created listener test in tickets service

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCreatedEvent, OrderStatus } from '@mmb8npm/common';
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 99,
+    userId: 'fakeuser',
+  });
+  await ticket.save();
+
+  // create a fake order data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    expiresAt: 'faketime',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // create a fake order message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, ticket };
+};
+
+it('sets the orderId of the ticket', async () => {
+  const { listener, data, msg, ticket } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure a ticket was created!
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket!.orderId).toEqual(data.id);
+});
+
+it('acknowledges the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack() function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
