d5a5fbe65ecfe5b50f13a46552f3bc1f9002ecb4
Leo <lcheung@swellbottle.com>
2022-01-29 07:43:31 -0500

    add a test to check version number get incremented on save

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -35,3 +35,22 @@ it('implements optimistic concurrency control', async () => {
 
   throw new Error('Should not reach this point');
 });
+
+it('increments the version number on multiple save', async () => {
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  // save the ticket to db
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
