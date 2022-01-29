42bd3468c998748050f5f8716fe778347798052b
Leo <lcheung@swellbottle.com>
2022-01-29 13:17:01 -0500

    finish up order cancelled listener logic

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,19 +1,35 @@
 import { Listener, OrderCancelledEvent, Subjects } from '@mmb8npm/common';
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
   readonly subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
   readonly queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCancelledEvent['data'], msg: Message) {
-    
-    {
-      id: string;
-      version: number;
-      ticket: {
-        id: string;
-      }
+  // remove orderId from Ticket to release the ticket which was reserved previously
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
     }
+
+    // remove orderId from ticket to release the reservation lock
+    ticket.set({ orderid: null });
+    await ticket.save();
+
+    // dispatch ticket updated event
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+      price: ticket.price,
+      title: ticket.title,
+      version: ticket.version,
+    });
+
+    msg.ack();
   }
 }
