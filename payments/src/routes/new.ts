813074b501667d963dcdd178502e79a6f3c82bda
Leo <lcheung@swellbottle.com>
2022-01-30 11:54:45 -0500

    add order checking validation before charging payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -5,6 +5,8 @@ import {
   validateRequest,
   BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
 } from '@mmb8npm/common';
 import { Order } from '../models/order';
 
@@ -19,7 +21,21 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({ succes: true });
+    const { token, orderId } = req.body;
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if(order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if(order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    res.send({ success: true });
   }
 );
 
