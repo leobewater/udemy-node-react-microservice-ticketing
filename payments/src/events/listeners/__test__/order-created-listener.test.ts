e91198825a71126872fb28bea4bbc84b824a32b6
Leo <lcheung@swellbottle.com>
2022-01-29 19:59:10 -0500

    add tests for order created listener in payments service

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCreatedEvent, OrderStatus } from '@mmb8npm/common';
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  // create a fake order data event
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: 'fakeuser',
+    expiresAt: 'faketime',
+    ticket: {
+      id: 'faketicketid',
+      price: 10,
+    },
+  };
+
+  // create a fake order message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it('replicates the order info', async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order!.id).toEqual(data.id);
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it('acknowledges the message', async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack() function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
