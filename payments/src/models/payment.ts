d06e86eff00dd0abcf90a5573a5f515bb042cadf
Leo <lcheung@swellbottle.com>
2022-01-30 13:34:04 -0500

    add payment in payments service for keeping track orderID and StripeId

---
+++ payments/src/models/payment.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@mmb8npm/common';
+// import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+// saving stripe payment and order to db
+
+// Typescript interfaces
+interface PaymentAttrs {
+  orderId: string;
+  stripeId: string;
+}
+
+interface PaymentDoc extends mongoose.Document {
+  orderId: string;
+  stripeId: string;
+  // version: number; // should add this for future usage
+}
+
+interface PaymentModel extends mongoose.Model<PaymentDoc> {
+  build(attrs: PaymentAttrs): PaymentDoc;
+}
+
+// Not TypeScript
+const paymentSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: true,
+    },
+    stripeId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+// override the mongoose __v with version and use plugin to increment the version number
+// paymentSchema.set('versionKey', 'version');
+// paymentSchema.plugin(updateIfCurrentPlugin);
+
+paymentSchema.statics.build = (attrs: PaymentAttrs) => {
+  return new Payment({
+    _id: attrs.id,
+    orderId: attrs.orderId,
+    stripeId: attrs.stripeId,
+  });
+};
+
+// mongoose Order model
+const Payment = mongoose.model<PaymentDoc, PaymentModel>('Payment', paymentSchema);
+
+export { Payment };
