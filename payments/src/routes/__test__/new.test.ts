557c0a9061c3da85d4b7c27c64d4c30f706117a9
Leo <lcheung@swellbottle.com>
2022-01-30 12:47:23 -0500

    test stripe payment with mock function

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+
+// use mock files
+jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
@@ -37,19 +41,19 @@ it('returns a 401 when purchasing an order that does not belong to the user', as
 });
 
 it('returns a 400 when purchasing a cancelled order', async () => {
-    const userId = new mongoose.Types.ObjectId().toHexString();
+  const userId = new mongoose.Types.ObjectId().toHexString();
 
-    // create order which already cancelled
-    const order = Order.build({
-      id: new mongoose.Types.ObjectId().toHexString(),
-      userId,
-      version: 0,
-      price: 20,
-      status: OrderStatus.Cancelled,
-    });
-    await order.save();
+  // create order which already cancelled
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+  await order.save();
 
-    await request(app)
+  await request(app)
     .post('/api/payments')
     .set('Cookie', global.signin(userId))
     .send({
@@ -57,4 +61,34 @@ it('returns a 400 when purchasing a cancelled order', async () => {
       orderId: order.id,
     })
     .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+
+  // create order
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  // use actual Stripe test token "tok_visa"
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+    // make sure we're sending the correct stripe argument
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(20 * 100);
+    expect(chargeOptions.currency).toEqual('usd');
 });
