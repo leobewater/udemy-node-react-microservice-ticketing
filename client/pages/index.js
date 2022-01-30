d27b43bf23bab96483900f40579cf243102a5f9f
Leo <lcheung@swellbottle.com>
2022-01-30 15:33:24 -0500

    client: elminiate duplicated get currentUser api calls

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,3 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({ currentUser }) => {
   return <h1>{currentUser ? 'You are signed in' : 'You are not signed in'}</h1>;
 };
@@ -7,10 +5,7 @@ const LandingPage = ({ currentUser }) => {
 // if _app.js has .getInitialProps(), this.getInitialProps won't be running by default
 // add condition in _app.js .getInitialProps() in order to run page component .getInitialProps()
 LandingPage.getInitialProps = async (context) => {
-  console.log('landing page!');
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+  return {};
 };
 
 export default LandingPage;
