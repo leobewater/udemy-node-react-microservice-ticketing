3a576120583d6ca8c0cff4df64eb28fcdd07bc39
Leo <lcheung@swellbottle.com>
2022-01-30 13:42:38 -0500

    fix mongoose timeout on testings

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -37,8 +37,8 @@ beforeEach(async () => {
 
 // stop mongodb after running all tests
 afterAll(async () => {
-  await mongo.stop();
   await mongoose.connection.close();
+  await mongo.stop();
 });
 
 /*
