4cc98dc59be38ea93b2558affb0228069bf68300
Leo <lcheung@swellbottle.com>
2022-01-31 07:32:21 -0500

    fix test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -10,7 +10,7 @@ it('responds with details about the current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(400);
+    .expect(200);
 
   // console.log(response.body); // { currentUser: null }
 
